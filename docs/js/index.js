238de328ba46bb3ba73e20ef6de29652f3deb9a3
parsa <parsafarashah@gmail.com>
2025-10-14 20:24:45 +0330

    add slider default loop

--- docs/js/index.js
+++ docs/js/index.js
@@ -9,10 +9,9 @@ btnRightDefault.classList.add("btnRightCss");
 btnLeftDefault.classList.add("btnLeftCss");
 freshWrapperDef.classList.add("wraperDef");
 
-
-_imagesWrapperDef.forEach((imageWrap)=>{
-    imageWrap.classList.add("imagesCss")
-})
+_imagesWrapperDef.forEach((imageWrap) => {
+  imageWrap.classList.add("imagesCss");
+});
 
 let flagRightDef = 0;
 let transRightNumbDef = 0;
@@ -52,3 +51,81 @@ btnLeftDefault.addEventListener("click", () => {
     });
   }
 });
+
+//slider default Loop
+
+let _imagesWrapperDefLoop = document.querySelectorAll(".freshSwiperDefLoop");
+let freshWrapperDefLoop = document.querySelector(".freshWrapperDefLoop");
+const btnRightDefaultLoop = document.getElementById("btnRightDefaultLoop");
+const btnLeftDefaultLoop = document.getElementById("btnLeftDefaultLoop");
+const TimeSpeedDefLoop = 1500;
+
+btnRightDefaultLoop.classList.add("btnRightCssLoop");
+btnLeftDefaultLoop.classList.add("btnLeftCssLoop");
+freshWrapperDefLoop.classList.add("wraperDefLoop");
+
+_imagesWrapperDefLoop.forEach((imageWrap) => {
+  imageWrap.classList.add("imagesCssLoop");
+});
+
+let flagRightDefLoop = 0;
+let transRightNumbDefLoop = 0;
+let flagRightClickDefLoop = 0;
+
+btnRightDefaultLoop.addEventListener("click", () => {
+  if (flagRightClickDefLoop == _imagesWrapperDefLoop.length - 1) {
+    btnRightDefaultLoop.removeEventListener("click", () => {});
+  } else {
+    flagRightClickDefLoop++;
+    transRightNumbDefLoop = transRightNumbDefLoop + flagRightDefLoop - 100;
+    _imagesWrapperDefLoop.forEach((item) => {
+      item.style.transition = "0.5s all";
+      item.style.transform = `translateX(${transRightNumbDefLoop}%)`;
+    });
+  }
+});
+
+let flagLeftClickDefLoop = 0;
+let flagLeftDefLoop = 0;
+
+btnLeftDefaultLoop.addEventListener("click", () => {
+  // if click
+  flagRightClickDefLoop--;
+  flagLeftClickDefLoop++;
+  //   if click too much and dont have picture
+  if (flagRightClickDefLoop < 0) {
+    btnLeftDefaultLoop.removeEventListener("click", () => {});
+    flagRightClickDefLoop = 0;
+    flagLeftClickDefLoop = 0;
+    transRightNumbDefLoop = 0;
+  } else {
+    transRightNumbDefLoop = transRightNumbDefLoop + flagLeftDefLoop + 100;
+    _imagesWrapperDefLoop.forEach((item) => {
+      item.style.transition = "0.5s all";
+      item.style.transform = `translateX(${transRightNumbDefLoop}%)`;
+    });
+  }
+});
+
+let goToRightLoop = true;
+let stateLoop = true;
+setInterval(() => {
+  if (goToRightLoop == true) {
+    if (stateLoop) {
+      btnRightDefaultLoop.click();
+      if (flagRightClickDefLoop === _imagesWrapperDefLoop.length - 1) {
+        stateLoop = false;
+      }
+    } else {
+      goToRightLoop = false;
+    }
+  } else {
+    // click on left
+    btnLeftDefaultLoop.click();
+    // click when image not exist
+    if (transRightNumbDefLoop == 0) {
+      goToRightLoop = true;
+      stateLoop = true;
+    }
+  }
+}, TimeSpeedDefLoop);
