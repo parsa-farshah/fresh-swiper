91e5f2295f7c7c9fcf389507fddd8a2f13ad3831
parsa <parsafarashah@gmail.com>
2025-10-14 19:41:53 +0330

    add code default slider

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,21 +1,30 @@
-let _images = document.querySelectorAll(".freshSwiper");
+// select
 
-// btn right
-
+let _imagesWrapperDef = document.querySelectorAll(".freshSwiperDef");
+let freshWrapperDef = document.querySelector(".freshWrapperDef");
 const btnRightDefault = document.getElementById("btnRightDefault");
 const btnLeftDefault = document.getElementById("btnLeftDefault");
+
+btnRightDefault.classList.add("btnRightCss");
+btnLeftDefault.classList.add("btnLeftCss");
+freshWrapperDef.classList.add("wraperDef");
+
+
+_imagesWrapperDef.forEach((imageWrap)=>{
+    imageWrap.classList.add("imagesCss")
+})
+
 let flagRightDef = 0;
 let transRightNumbDef = 0;
-
 let flagRightClickDef = 0;
 
 btnRightDefault.addEventListener("click", () => {
-  if (flagRightClickDef == _images.length - 1) {
+  if (flagRightClickDef == _imagesWrapperDef.length - 1) {
     btnRightDefault.removeEventListener("click", () => {});
   } else {
     flagRightClickDef++;
     transRightNumbDef = transRightNumbDef + flagRightDef - 100;
-    _images.forEach((item) => {
+    _imagesWrapperDef.forEach((item) => {
       item.style.transition = "0.5s all";
       item.style.transform = `translateX(${transRightNumbDef}%)`;
     });
@@ -24,6 +33,7 @@ btnRightDefault.addEventListener("click", () => {
 
 let flagLeftClickDef = 0;
 let flagLeftDef = 0;
+
 btnLeftDefault.addEventListener("click", () => {
   // if click
   flagRightClickDef--;
@@ -36,7 +46,7 @@ btnLeftDefault.addEventListener("click", () => {
     transRightNumbDef = 0;
   } else {
     transRightNumbDef = transRightNumbDef + flagLeftDef + 100;
-    _images.forEach((item) => {
+    _imagesWrapperDef.forEach((item) => {
       item.style.transition = "0.5s all";
       item.style.transform = `translateX(${transRightNumbDef}%)`;
     });
