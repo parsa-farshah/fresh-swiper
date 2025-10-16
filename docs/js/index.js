f45e0f0b8de0f882758e752c7640aa5452fca462
parsa <parsafarashah@gmail.com>
2025-10-16 11:42:52 +0330

    add effect overflow page

--- docs/js/index.js
+++ docs/js/index.js
@@ -510,7 +510,7 @@ btnRightEffectOverflow.addEventListener("click", () => {
 });
 
 btnLeftEffectOverflow.addEventListener("click", () => {
-    let imgEff1 = document.querySelector(".effect1");
+  let imgEff1 = document.querySelector(".effect1");
   let imgEff2 = document.querySelector(".effect2");
   let imgEff3 = document.querySelector(".effect3");
 
@@ -534,3 +534,93 @@ btnLeftEffectOverflow.addEventListener("click", () => {
     imgEff1.setAttribute("data-tartib", "2");
   }
 });
+
+///////////////////////////////////////////////////////// efect overflow end
+
+///////////////////////////////////////////////////////// efect overflow Loop
+
+let _imagesWrapperEffectOverflowLp = document.querySelectorAll(
+  ".freshSwiperEffectOverflowLp"
+);
+let freshWrapperEffectOverflowLp = document.querySelector(
+  ".freshWrapperEffectOverflowLp"
+);
+const btnRightEffectOverflowLp = document.getElementById(
+  "btnRightEffectOverflowLp"
+);
+const btnLeftEffectOverflowLp = document.getElementById(
+  "btnLeftEffectOverflowLp"
+);
+const TimeSpeedEffectOverflowLp = 2500;
+
+btnRightEffectOverflowLp.classList.add("btnRightCssEffectOverflowLp");
+btnLeftEffectOverflowLp.classList.add("btnLeftCssEffectOverflowLp");
+freshWrapperEffectOverflowLp.classList.add("wraperEffectOverflowLp");
+
+_imagesWrapperEffectOverflowLp.forEach((imageWrap) => {
+  imageWrap.classList.add("imagesCssEffectOverflowLp");
+});
+
+let flagRightEffectOverflowLp = 0;
+let transRightNumbEffectOverflowLp = 0;
+let flagRightClickEffectOverflowLp = 0;
+
+btnRightEffectOverflowLp.addEventListener("click", () => {
+  let imgEffLp1 = document.querySelector(".effectLp1");
+  let imgEffLp2 = document.querySelector(".effectLp2");
+  let imgEffLp3 = document.querySelector(".effectLp3");
+
+  let dataTartibLp1 = imgEffLp1.getAttribute("data-tartib");
+  let dataTartibLp2 = imgEffLp2.getAttribute("data-tartib");
+  let dataTartibLp3 = imgEffLp3.getAttribute("data-tartib");
+
+  if (dataTartibLp2 == 2) {
+    imgEffLp2.classList.remove("effectLp2");
+    imgEffLp2.classList.add("effectLp1");
+    imgEffLp2.setAttribute("data-tartib", "1");
+  }
+  if (dataTartibLp3 == 3) {
+    imgEffLp3.classList.remove("effectLp3");
+    imgEffLp3.classList.add("effectLp2");
+    imgEffLp3.setAttribute("data-tartib", "2");
+  }
+  if (dataTartibLp1 == 1) {
+    imgEffLp1.classList.remove("effectLp1");
+    imgEffLp1.classList.add("effectLp3");
+    imgEffLp1.setAttribute("data-tartib", "3");
+  }
+});
+
+btnLeftEffectOverflowLp.addEventListener("click", () => {
+  let imgEffLp1 = document.querySelector(".effectLp1");
+  let imgEffLp2 = document.querySelector(".effectLp2");
+  let imgEffLp3 = document.querySelector(".effectLp3");
+
+  let dataTartibLp1 = imgEffLp1.getAttribute("data-tartib");
+  let dataTartibLp2 = imgEffLp2.getAttribute("data-tartib");
+  let dataTartibLp3 = imgEffLp3.getAttribute("data-tartib");
+
+  if (dataTartibLp2 == 2) {
+    imgEffLp2.classList.remove("effectLp2");
+    imgEffLp2.classList.add("effectLp3");
+    imgEffLp2.setAttribute("data-tartib", "3");
+  }
+  if (dataTartibLp3 == 3) {
+    imgEffLp3.classList.remove("effectLp3");
+    imgEffLp3.classList.add("effectLp1");
+    imgEffLp3.setAttribute("data-tartib", "1");
+  }
+  if (dataTartibLp1 == 1) {
+    imgEffLp1.classList.remove("effectLp1");
+    imgEffLp1.classList.add("effectLp2");
+    imgEffLp1.setAttribute("data-tartib", "2");
+  }
+});
+
+// loop
+
+setInterval(() => {
+  btnRightEffectOverflowLp.click();
+}, TimeSpeedEffectOverflowLp);
+
+///////////////////////////////////////////////////////// efect overflow Loop end
